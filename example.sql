40c80885e0f6b3cbeb475671d4efc5f5e8944638
Juriy <9090858496@mail.ru>
2019-09-17 19:11:06 +0500

    example_dump in sample

--- example.sql
+++ example.sql
@@ -2,3 +2,11 @@ CREATE DATABASE IF NOT EXISTS example;
 USE example;
 CREATE TABLE  IF NOT EXISTS  users (id  int, name  char);
 
+\! mysqldump  example > example_dump.sql
+
+
+CREATE DATABASE  IF NOT EXISTS sample;
+USE  sample;
+
+\! mysql sample < example_dump.sql;
+
