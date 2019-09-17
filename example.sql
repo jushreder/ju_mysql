0d78f20091c40a1f0449aaaea9bb2f628ecd15d3
Juriy <9090858496@mail.ru>
2019-09-17 19:23:39 +0500

    name users varchar 50

--- example.sql
+++ example.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS example;
 USE example;
-CREATE TABLE  IF NOT EXISTS  users (id  int, name  char);
+CREATE TABLE  IF NOT EXISTS  users (id  int, name  varchar(50));
 
 \! mysqldump  example > example_dump.sql
 
